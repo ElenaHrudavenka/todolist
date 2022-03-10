e47bd31a54a14fa3567c2eac8b01ccfcc5813ddd
ElenaHrudavenka <kisseli@mail.ru>
2022-03-10 12:29:50 +0300

    Create empty-reducer.ts

---
+++ src/State/empty-reducer.ts
@@ -0,0 +1,33 @@
+import { TasksStateType } from "../App";
+
+
+type Action1Type = {
+    type: '1'
+    id: string
+}
+type Action2Type = {
+    type: '2'
+    newTodolistTitle: string
+}
+
+//юнион тип
+type ActionsType = Action1Type | Action2Type
+
+export const todolistsReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
+    switch (action.type) {
+        case '1':
+            return {...state}
+        case '2':
+            return {...state}
+        default:
+            throw new Error("I don't understand this type")
+    }
+}
+
+export const action1AC = (todolistID1: string): Action1Type => {
+    return {type: '1', id: todolistID1}
+}
+
+export const action2AC = (newTodolistTitle: string): Action2Type => {
+    return {type: '2', newTodolistTitle}
+}
