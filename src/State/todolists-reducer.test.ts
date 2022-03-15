bc7c52cc2d360a10e64eb56550b05adcd1b51a05
ElenaHrudavenka <kisseli@mail.ru>
2022-03-15 16:12:00 +0300

    Update todolists-reducer.test.ts
    10 - Redax

--- src/State/todolists-reducer.test.ts
+++ src/State/todolists-reducer.test.ts
@@ -7,27 +7,27 @@ import {
     todolistsReducer
 } from "./todolists-reducer";
 
-let todolistID1: string
-let todolistID2: string
+let todolistId1: string
+let todolistId2: string
 let startState: Array<TodolistsType>
 
     beforeEach(() => {
-    todolistID1 = v1();
-    todolistID2 = v1();
+    todolistId1 = v1();
+    todolistId2 = v1();
 
     startState = [
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
     ]
 })
 
 
 test('correct todolist should be removed', () => {
 
-    const endState = todolistsReducer(startState, removeTodolistAC(todolistID1))
+    const endState = todolistsReducer(startState, removeTodolistAC(todolistId1))
 
     expect(endState.length).toBe(1)
-    expect(endState[0].id).toBe(todolistID2)
+    expect(endState[0].id).toBe(todolistId2)
 })
 
 test('correct todoliist should be added', () => {
@@ -46,10 +46,10 @@ test('correct todolist should change its name', () => {
     /*    const action = {
             type: 'CHANGE-TODOLIST-TITLE' as const, //или явно указать что это константа, а не string
             changeTodolistTitle,
-            id: todolistID2
+            id: todolistId2
         }
         const endState = todolistsReducer(startState, action)*/
-    const endState = todolistsReducer(startState, changeTodolistAC(todolistID2, changeTodolistTitle))
+    const endState = todolistsReducer(startState, changeTodolistAC(todolistId2, changeTodolistTitle))
 
     expect(endState[0].title).toBe("What to learn")
     expect(endState[1].title).toBe(changeTodolistTitle)
@@ -60,11 +60,11 @@ test('correct filter of todolist should be changed', () => {
 
     /*    const action: ChangeTodolistFilterActionType = {  //или явно указать тип экшена
             type: 'CHANGE-TODOLIST-FILTER',
-            id: todolistID2,
+            id: todolistId2,
             newTodolistFilter
         }
         const endState = todolistsReducer(startState, action)*/
-    const endState = todolistsReducer(startState, changeTodolistFilterAC(todolistID2, newTodolistFilter))
+    const endState = todolistsReducer(startState, changeTodolistFilterAC(todolistId2, newTodolistFilter))
 
     expect(endState[0].filter).toBe("all")
     expect(endState[1].filter).toBe(newTodolistFilter)
