3af0b50710ea9d8fe234d304c93c373c706d7059
ElenaHrudavenka <kisseli@mail.ru>
2022-03-15 16:11:40 +0300

    Create store.ts
    10 - Redax

---
+++ src/State/store.ts
@@ -0,0 +1,17 @@
+import {combineReducers, createStore} from "redux";
+import {tasksReducer} from "./tasks-reducer";
+import {todolistsReducer} from "./todolists-reducer";
+
+//объединяем наши редюсеры в единственный объект-состояния
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolist: todolistsReducer,
+})
+
+//создаем store
+export const store = createStore(rootReducer)
+// автоматически определяем тип нашего объекта состояния (нашего стейта)
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// @ts-ignore
+window.store = store;
